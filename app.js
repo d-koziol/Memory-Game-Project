d38c26aaaafb2922567b9cce7d0462e3a14cc42f
damcio <d.p.koziol@gmail.com>
2023-05-05 12:50:52 +0200

    createBoard function added.

--- app.js
+++ app.js
@@ -24,11 +24,40 @@ const cardArray = [
     img: "images/pizza.png",
   },
   {
-    name: "white",
-    img: "images/white.png",
+    name: "fries",
+    img: "images.fries.png",
   },
   {
-    name: "blank",
-    img: "images/blank.png",
+    name: "cheeseburger",
+    img: "images/cheeseburger.png",
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png",
   },
 ];
+cardArray.sort(() => 0.5 - Math.random());
+
+const gridDisplay = document.querySelector("#grid");
+
+function createBoard() {
+  for (let i = 0; i < 10; i += 1) {
+    const card = document.createElement("img");
+    card.setAttribute("src", "images/blank.png");
+    card.setAttribute("data-id", i);
+    gridDisplay.appendChild(card);
+  }
+}
+createBoard();
